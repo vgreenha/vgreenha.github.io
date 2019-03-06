b9b699b42832df64d3f256f15619e8c193a7bbe6
vgreenha <gre18004@byui.edu>
2019-03-06 11:19:24 -0700

    sldkgn

--- weather/js/local.js
+++ weather/js/local.js
@@ -1,13 +1,13 @@
-fetch(URL)
- .then(function(response) {
-   if(response.ok){
-     return response.json();
-   }
-   throw new ERROR('Network response was not OK.');
- })
- .then(function(data){
-   ... do something with the JavaScript object ...
- })
- .catch(function(error){
-console.log('There was a fetch problem: ', error.message);
- })
+// fetch(URL)
+//  .then(function(response) {
+//    if(response.ok){
+//      return response.json();
+//    }
+//    throw new ERROR('Network response was not OK.');
+//  })
+//  .then(function(data){
+//    ... do something with the JavaScript object ...
+//  })
+//  .catch(function(error){
+// console.log('There was a fetch problem: ', error.message);
+//  })
