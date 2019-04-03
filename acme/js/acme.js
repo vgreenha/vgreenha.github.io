30840d33ba410d9846a4da8ec3d9b509ec34ad25
vgreenha <gre18004@byui.edu>
2019-04-03 10:07:41 -0600

    ;kn

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -1,3 +1,9 @@
 /* *************************************
 *  Acme Functions
 ************************************* */
+console.log('Its connected');
+var page ='';
+function changeToHome(){
+    console.log('Button Worked')
+    mainContent.setAttribute('class', '')
+}
